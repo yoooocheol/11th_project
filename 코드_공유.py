b456897349b1ff9a07064b16e719f2e9a3c4a699
test <test@test.com>
2026-07-23 15:39:26 +0900

    XGB 모델링 작업 수행

--- 코드_공유.py
+++ 코드_공유.py
@@ -17,7 +17,14 @@ wine = load_wine()
 # X, y 데이터를 test size는 0.2, random_state 값은 42로 하여 train 데이터와 test 데이터로 분할합니다.
 
 ''' 코드 작성 바랍니다 '''
+wine_df = pd.DataFrame(wine.data, columns=wine.feature_names)
 
+wine_df['target'] = wine.target
+
+X = wine_df.drop(columns='target')
+y = wine_df['target']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 ####### A 작업자 작업 수행 #######
 
@@ -28,4 +35,22 @@ wine = load_wine()
 ####### B 작업자 작업 수행 #######
 
 ''' 코드 작성 바랍니다 '''
+from xgboost import XGBClassifier
+data_xgb_model = XGBClassifier(random_state= 42, eval_metric = 'mlogloss')
+data_xgb_parameters = {'max_depth':[3,5,7,9,15], 'learning_rate': [0.1, 0.01, 0.001], 'n_estimators':[50, 100, 200, 300]}
+xgb_grid = GridSearchCV(estimator= data_xgb_model, param_grid=data_xgb_parameters, scoring = 'accuracy', cv = 5)
+xgb_grid.fit(X_train, y_train)
+print('Best parameters', xgb_grid.best_params_)
+print('Best accuracy', xgb_grid.best_score_)
 
+xgb_best_model = xgb_grid.best_estimator_
+xgb_importance = pd.DataFrame({'Feature':X.columns, 'importance':xgb_best_model.feature_importances_})
+
+plt.figure(figsize = (12,5))
+plt.bar(xgb_importance['Feature'], xgb_importance['importance'])
+plt.title('Feature Importance')
+plt.xlabel('Feature')
+plt.ylabel('importance')
+plt.xticks(rotation = 45)
+plt.tight_layout()
+plt.show()
